82dbbbb7c9a55223b979134919a246092dca256a
Favour Felix <felixfavour0@gmail.com>
2022-01-09 22:04:02 +0400

    added env variables and connected DB

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// Require environment variables
+require('dotenv').config()
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
@@ -8,9 +11,12 @@ app.use(bodyParser.json())
 // ROUTER
 const router = express.Router()
 router.get('/', (req,res) => {
-  console.log('I reached here.')
   res.json({ message: 'Welcome to Easy Internships' })
 })
+
+// DB SETUP
+const mongoose = require('mongoose')
+mongoose.connect(`${process.env.DB_HOST}://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5g703.mongodb.net/EASYiNTERNSHIPS?retryWrites=true&w=majority`)
 
 app.use('/api/v1', router)
 
