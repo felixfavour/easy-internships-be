32feb2d7e7b5700cc49f37b8aecbf35913a07851
Favour Felix <felixfavour0@gmail.com>
2022-02-19 21:14:47 +0400

    restructured codebase architecture and added eslint

---
+++ config/database.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose'
+
+// Require environmental variables
+require('dotenv').config()
+
+// Method to connect to MongoDB database
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(`${process.env.DB_HOST}://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5g703.mongodb.net/EASYiNTERNSHIPS`)
+    console.log('DB Connection Successful')
+  } catch (err) {
+    console.error(`DB Connnection failed: ${err}`)
+  }
+}
+
+// Method to disconnect from MongoDB Database
+export const closeDB = async () => mongoose.disconnect()
+
+// DB SETUP
--- server.js
+++ server.js
@@ -1,29 +1,5 @@
-// Require environment variables
-require('dotenv').config()
+import app from './app'
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-
-// ROUTER
-const router = express.Router()
-router.get('/', (req,res) => {
-  res.json({ message: 'Welcome to Easy Internships' })
-})
-
-// DB SETUP
-const mongoose = require('mongoose')
-mongoose.connect(`${process.env.DB_HOST}://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5g703.mongodb.net/EASYiNTERNSHIPS?retryWrites=true&w=majority`)
-
-// CREATE USER INSTANCE
-const User = require('./app/models/User')
-
-app.use('/api/v1', router)
-
-// ALLOCATE SERVER HOST
 const port = process.env.port || 5000
 app.listen(port)
 console.log(`App has started on port: ${port}`)
