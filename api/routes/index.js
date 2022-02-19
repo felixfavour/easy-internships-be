0f191729297905579278d095117ed485e91949e1
Favour Felix <felixfavour0@gmail.com>
2022-02-19 21:50:00 +0400

    registered first route with controller in app.js

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,5 +1,7 @@
 import { Router } from 'express';
-import get from '../../controllers';
+import get from '../../controllers/index.js';
 
 const router = Router()
 router.get('/', get)
+
+export default router
--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 import express from 'express'
+import initialRoute from './api/routes/index.js'
 
 const app = express()
+
+// Register routes
+app.use('', initialRoute)
 
 export default app
