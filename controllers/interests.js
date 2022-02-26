8e3320dfac01032cf8a1a2ae1d0c9644815f9f09
Favour Felix <felixfavour0@gmail.com>
2022-02-26 12:20:47 +0400

    added controllers for interests

---
+++ controllers/interests.js
@@ -0,0 +1,19 @@
+// Get User Interests
+export const getUserInterests = async (req, res) => {
+  try {
+    //
+  } catch (err) {
+    console.error(`ERROR from ${req.url}: ${err}`)
+    res.status(400).json(err)
+  }
+}
+
+// Add/Remove from User Interests
+export const updateUserInterests = async (req, res) => {
+  try {
+    //
+  } catch (err) {
+    console.error(`ERROR from ${req.url}: ${err}`)
+    res.status(400).json(err)
+  }
+}
