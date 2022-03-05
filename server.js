d15285438ac01a4a3925ef188affc26c10ad8579
Favour Felix <felixfavour0@gmail.com>
2022-03-05 23:40:46 +0400

    changed port to PORT

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import app from './app.js'
 import { connectDB } from './config/database.js'
 
-const port = process.env.port || 5000
+const port = process.env.PORT || 5000
 
 await connectDB()
 app.listen(port, () => {
