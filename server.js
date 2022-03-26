5bd325ac516e26c74606378c05eeaf675da0427f
Favour Felix <favour@acumen.digital>
2022-03-26 14:05:42 +0400

    updated port number and package-lock

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import app from './app.js'
 import { connectDB } from './config/database.js'
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 4000
 
 await connectDB()
 app.listen(port, () => {
